c7f407c479fecb306203a6d06d45cde2228b1c2d
palomasfd <112178872+palomasfd@users.noreply.github.com>
2024-10-09 08:52:09 -0300

    Add files via upload

---
+++ graficos/common.js
@@ -0,0 +1,22 @@
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
+}
+
+const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+}
+
+function criarGrafico(data, layout) {
+    const grafico = document.createElement('div');
+    grafico.className = 'grafico';
+    document.getElementById('graficos-container').appendChild(grafico);
+    const config = {
+        responsive: true,
+        displayModeBar: false
+    }
+    Plotly.newPlot(grafico, data, layout, config);
+}
+
+export { getCSS, tickConfig, criarGrafico }
