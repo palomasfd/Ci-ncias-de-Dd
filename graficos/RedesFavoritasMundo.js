8c764db71710635b563301c215fd6b3d31c399f4
palomasfd <112178872+palomasfd@users.noreply.github.com>
2024-12-04 08:46:38 -0300

    Add files via upload

--- graficos/RedesFavoritasMundo.js
+++ graficos/RedesFavoritasMundo.js
@@ -11,27 +11,27 @@ async function redesFavoritasMundo() {
         {
             values: valores,
             labels: redes,
-            type: 'pie',
-            textinfo: 'label+percent'
+            type: "pie",
+            textinfo: "label+percent"
         }
     ]
 
     const layout = {
-        plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
+        plot_bgcolor: getCSS("--bg-color"),
+        paper_bgcolor: getCSS("--bg-color"),
         height: 700,
         title: {
-            text: 'Redes sociais que os usuários mais gostam',
+            text: "Redes sociais que os usuários mais gostam",
             x: 0,
             font: {
-                color: getCSS('--primary-color'),
-                family: getCSS('--font'),
+                color: getCSS("--primary-color"),
+                family: getCSS("--font"),
                 size: 30
             }
         },
         legend: {
             font: {
-                color: getCSS('--primary-color'),
+                color: getCSS("--primary-color"),
                 size: 16
             }
         }
